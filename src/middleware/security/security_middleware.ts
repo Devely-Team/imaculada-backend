2661c872d3d195751719c733268197d58e307f76
Rodrigo Santos de Souza <souza.rodrigo61@gmail.com>
2023-02-16 20:49:42 -0300

    fix: improves error handling during token breaking

--- src/middleware/security/security_middleware.ts
+++ src/middleware/security/security_middleware.ts
@@ -1,4 +1,6 @@
 import { NextFunction, Request, Response } from "express";
+import { BadRequestError } from "../../core/error/bad_request_error";
+import { BaseErrorCodes } from "../../core/error/base_error";
 
 import { verifyToken } from "../../modules/user/auth/infra/services/auth_verify_token";
 
@@ -7,14 +9,24 @@ const securityMiddleware = (
   response: Response,
   next: NextFunction,
 ) => {
-  const token = request.headers.authorization as string;
-  const tokenSplit = token.split(" ")[1];
-  return verifyToken({
-    token: tokenSplit,
-    request,
-    response,
-    next,
-  });
+  try {
+    const token = request.headers.authorization as string;
+    const tokenSplit = token.split(" ")[1];
+    return verifyToken({
+      token: tokenSplit,
+      request,
+      response,
+      next,
+    });
+  } catch (error) {
+    let message = "Unknown Error";
+    if (error instanceof Error) message = error.message;
+    return new BadRequestError(
+      BaseErrorCodes.objectValidation,
+      "Erro durante a quebra do user token",
+      message,
+    );
+  }
 };
 
 export { securityMiddleware };
