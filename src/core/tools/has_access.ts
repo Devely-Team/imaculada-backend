9dc927a4f1aa7fb0a74685c68c86cfecc991dc9f
Rodrigo Santos de Souza <souza.rodrigo61@gmail.com>
2023-03-03 14:26:32 -0300

    fixedd

--- src/core/tools/has_access.ts
+++ src/core/tools/has_access.ts
@@ -6,7 +6,7 @@ import {
 } from "../../middleware/error/on_error";
 import { Account } from "../../modules/user/account/domain/model/account";
 
-function hasAccess<T>(
+async function hasAccess<T>(
   request: Request,
   response: Response,
   next: NextFunction,
@@ -20,7 +20,7 @@ function hasAccess<T>(
     const usr = user as Account;
     const result = usr.profile.filter(e => e.profile === profile);
     if (result.length > 0) {
-      return action;
+      return await action;
     }
     return onAccessDenied(usr.username, access, request, response);
   }
