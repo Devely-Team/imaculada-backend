9df251dde5e882e512b73a8344fbaa8060c3797c
Rodrigo Santos de Souza <31740435+SouzaRodrigo61@users.noreply.github.com>
2023-03-15 10:10:52 -0300

    feat: middleware with sentry (#25)
    * feat: admin update flow
    
    * feat: middleware with sentry

--- src/middleware/middleware.ts
+++ src/middleware/middleware.ts
@@ -1,13 +1,63 @@
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 import bodyParser from "body-parser";
 import cors, { CorsOptions } from "cors";
 import express, { Express, Request } from "express";
 
+import { StatusCodes } from "../core/utils/http_status_code";
 import { loggerMiddleware } from "./config/logger";
 import { logErrorMiddleware } from "./error/error_handler";
 import { routes } from "./router/routes";
 
 class Middleware {
   execute(app: Express): void {
+    const sentryDSN = process.env.SENTRY_CONNECTION;
+
+    Sentry.init({
+      dsn: sentryDSN,
+      integrations: [
+        // enable HTTP calls tracing
+        new Sentry.Integrations.Http({ tracing: true }),
+        // enable Express.js middleware tracing
+        new Tracing.Integrations.Express({
+          // to trace all requests to the default router
+          app,
+          // alternatively, you can specify the routes you want to trace:
+          // router: someRouter,
+        }),
+      ],
+
+      // We recommend adjusting this value in production, or using tracesSampler
+      // for finer control
+      tracesSampleRate: 0.8,
+    });
+
+    // RequestHandler creates a separate execution context using domains, so that every
+    // transaction/span/breadcrumb is attached to its own Hub instance
+    app.use(Sentry.Handlers.requestHandler());
+    // TracingHandler creates a trace for every incoming request
+    app.use(Sentry.Handlers.tracingHandler());
+
+    // the rest of your app
+    app.use(
+      Sentry.Handlers.errorHandler({
+        shouldHandleError(error) {
+          switch (error.status) {
+            case StatusCodes.BadRequest:
+            case StatusCodes.Unauthorized:
+            case StatusCodes.Forbidden:
+            case StatusCodes.NotFound:
+            case StatusCodes.Conflit:
+            case StatusCodes.Error:
+            case StatusCodes.Not_Implemented:
+              return true;
+            default:
+              return false;
+          }
+        },
+      }),
+    );
+
     const allowlist = ["https://imaculada-conceicao-app.web.app"];
     const corsOptionsDelegate = (
       req: Request,
