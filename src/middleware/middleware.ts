9accdef7c8257d5ad7fe7864f585f1bd8d29eb33
Rodrigo Santos de Souza <souza.rodrigo61@gmail.com>
2023-03-19 20:40:33 -0300

    improvment: update acquirer

--- src/middleware/middleware.ts
+++ src/middleware/middleware.ts
@@ -19,17 +19,12 @@ class Middleware {
         // enable HTTP calls tracing
         new Sentry.Integrations.Http({ tracing: true }),
         // enable Express.js middleware tracing
-        new Tracing.Integrations.Express({
-          // to trace all requests to the default router
-          app,
-          // alternatively, you can specify the routes you want to trace:
-          // router: someRouter,
-        }),
+        new Tracing.Integrations.Express({ app }),
       ],
 
       // We recommend adjusting this value in production, or using tracesSampler
       // for finer control
-      tracesSampleRate: 0.8,
+      tracesSampleRate: 1.0,
     });
 
     // RequestHandler creates a separate execution context using domains, so that every
