2d1509cedc3ec5ffd13919bbe23ae34ab24c6e4d
Rodrigo Santos de Souza <souza.rodrigo61@gmail.com>
2023-05-08 13:49:45 -0300

    feat: add database client verify

--- src/core/prisma/prisma_client.ts
+++ src/core/prisma/prisma_client.ts
@@ -1,7 +1,16 @@
 import { PrismaClient } from "@prisma/client";
 
 class DatabaseClient {
-  constructor(private prisma: PrismaClient = new PrismaClient()) {}
+  constructor(private prisma: PrismaClient = new PrismaClient()) {
+    this.prisma
+      .$connect()
+      .then(() => {
+        console.log("DatabaseClient: get clientPrisma: connected");
+      })
+      .catch(error => {
+        console.log("DatabaseClient: get clientPrisma: error: ", error);
+      });
+  }
 
   get clientPrisma() {
     return this.prisma;
