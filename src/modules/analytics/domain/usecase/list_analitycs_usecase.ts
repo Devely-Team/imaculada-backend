07090fc509000c3840064ae33548452a96a970ad
Rodrigo Santos de Souza <souza.rodrigo61@gmail.com>
2023-06-08 08:29:06 -0300

    feat: get all quotas analitycs

--- src/modules/analytics/domain/usecase/list_analitycs_usecase.ts
+++ src/modules/analytics/domain/usecase/list_analitycs_usecase.ts
@@ -1,22 +1,51 @@
 import { Success } from "../../../../core/tools/result_type";
+import { campaignPaymentDate } from "../../../booklet/infra/data/campaign_payment_date";
 import { AnalitycsRepository } from "../../infra/repositories/analitycs_repository";
 
 export class ListAnalitycsUseCase {
   static async execute(quota?: number) {
-    const countedPayd = await AnalitycsRepository.countedBookletHasPayment(
-      quota,
-    );
+    if (quota) {
+      const countedPayd = await AnalitycsRepository.countedBookletHasPayment(
+        quota,
+      );
 
-    const countedNotPayd =
-      await AnalitycsRepository.countedBookletNotHasPayment(quota);
+      const countedNotPayd =
+        await AnalitycsRepository.countedBookletNotHasPayment(quota);
 
-    const countedTotal = await AnalitycsRepository.countedBookletTotal(quota);
+      const countedTotal = await AnalitycsRepository.countedBookletTotal(quota);
 
-    return Success({
-      countedPayd: countedPayd.ok ? countedPayd.value : 0,
-      countedNotPayd: countedNotPayd.ok ? countedNotPayd.value : 0,
-      countedTotal: countedTotal.ok ? countedTotal.value : 0,
-      quota,
+      return Success({
+        countedPayd: countedPayd.ok ? countedPayd.value : 0,
+        countedNotPayd: countedNotPayd.ok ? countedNotPayd.value : 0,
+        countedTotal: countedTotal.ok ? countedTotal.value : 0,
+        quota,
+      });
+    }
+
+    const result = campaignPaymentDate.map(async data => {
+      const countedPayd = await AnalitycsRepository.countedBookletHasPayment(
+        data.quota,
+      );
+
+      const countedNotPayd =
+        await AnalitycsRepository.countedBookletNotHasPayment(data.quota);
+
+      const countedTotal = await AnalitycsRepository.countedBookletTotal(
+        data.quota,
+      );
+
+      return {
+        countedPayd: countedPayd.ok ? countedPayd.value : 0,
+        countedNotPayd: countedNotPayd.ok ? countedNotPayd.value : 0,
+        countedTotal: countedTotal.ok ? countedTotal.value : 0,
+        quota: data.quota,
+      };
     });
+
+    return Promise.all(result)
+      .then(result => Success(result))
+      .catch(() => {
+        return Success([]);
+      });
   }
 }
