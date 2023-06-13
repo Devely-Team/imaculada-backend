58b2b162654cc87b4416ac2bf81561fce5829d1a
Rodrigo Santos de Souza <souza.rodrigo61@gmail.com>
2023-06-13 09:40:04 -0300

    fix: change methods not used

--- src/modules/booklet/domain/command/create_booklet_command.ts
+++ src/modules/booklet/domain/command/create_booklet_command.ts
@@ -2,6 +2,7 @@ import { BadRequestError } from "../../../../core/error/bad_request_error";
 import { BaseErrorCodes } from "../../../../core/error/base_error";
 import { hasAccess } from "../../../../core/tools/has_access";
 import { Failure } from "../../../../core/tools/result_type";
+import { findByIdAcquirer } from "../../../acquirer/infra/repositories/acquirer_repository";
 import { FindByIdCampaignUseCase } from "../../../campaing/domain/usecase/find_by_id_campaign_usecase";
 import { Account } from "../../../user/account/domain/model/account";
 import { CreateBookletDTO } from "../dto/create_booklet_dto";
@@ -28,7 +29,7 @@ export class CreateBookletCommand {
       return campaign;
     }
 
-    const acquirer = await AcquirerReposity.findById(input.acquirerId);
+    const acquirer = await findByIdAcquirer(input.acquirerId);
     if (acquirer.ok === false) {
       return acquirer;
     }
